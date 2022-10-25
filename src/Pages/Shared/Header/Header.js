fdb4d5c19c816c1f92b551c9a8aee9f4a67b2252
Sujoy Paul <freelancersujoy@gmail.com>
2022-10-25 21:59:08 +0600

    title and favicon logo all seted

--- src/Pages/Shared/Header/Header.js
+++ src/Pages/Shared/Header/Header.js
@@ -2,29 +2,25 @@ import React from 'react';
 import Container from 'react-bootstrap/Container';
 import Nav from 'react-bootstrap/Nav';
 import Navbar from 'react-bootstrap/Navbar';
-import NavDropdown from 'react-bootstrap/NavDropdown';
+import logo from '../../../assets/brand/logo.png'
 
 const Header = () => {
     return (
-        <Navbar collapseOnSelect expand="lg" bg="light" variant="light">
+        <Navbar collapseOnSelect expand="lg" bg="light" variant="light" >
       <Container>
-        <Navbar.Brand href="#home">DEEP CODER KNOWLEDGE</Navbar.Brand>
+        <Navbar.Brand className="d-flex align-items-center">
+        <img
+              src={logo}
+              height="40"
+              alt="React Bootstrap logo"
+            />
+            <p className='m-0'>DEEP CODER KNOWLEDGE</p>
+        </Navbar.Brand>
         <Navbar.Toggle aria-controls="responsive-navbar-nav" />
         <Navbar.Collapse id="responsive-navbar-nav">
           <Nav className="me-auto">
             <Nav.Link href="#features">Features</Nav.Link>
             <Nav.Link href="#pricing">Pricing</Nav.Link>
-            <NavDropdown title="Dropdown" id="collasible-nav-dropdown">
-              <NavDropdown.Item href="#action/3.1">Action</NavDropdown.Item>
-              <NavDropdown.Item href="#action/3.2">
-                Another action
-              </NavDropdown.Item>
-              <NavDropdown.Item href="#action/3.3">Something</NavDropdown.Item>
-              <NavDropdown.Divider />
-              <NavDropdown.Item href="#action/3.4">
-                Separated link
-              </NavDropdown.Item>
-            </NavDropdown>
           </Nav>
           <Nav>
             <Nav.Link href="#deets">More deets</Nav.Link>
